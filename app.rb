9f396080f9a0984f3254747667e7f952ad61e7a6
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-26 22:41:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-
+  get '/' do 
+    @pirates = Pirate.all
+    erb :'pirates/index'
   end
+  
+  post '/pirates' do
+    @pirate = Student.new(params[:pirate])
+ 
+    params[:pirate][:ships].each do |details|
+      Ship.new(details)
+    end
+ 
+    @ships = Ship.all
+ 
+    erb :show
+  end 
+end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_reader :name, :type, :attributes
+  
+  SHIPS = []
+  
+  def initialize(args)
+    @name = args[:name] 
+    @type = args[:type]
+    @attributes = args[:attributes]
+    SHIPS << self
+  end 
+  
+  def self.all 
+    COURSES
+  end 
+  
+  def self.clear_all 
+    COURSES.clear 
+  end 
+  
 end
